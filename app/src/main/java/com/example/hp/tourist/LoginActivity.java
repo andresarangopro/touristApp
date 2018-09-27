2e0f075105082a826dea1f4b2f91b6c3a93aaa43
andresarangopro <andresarangopro@gmail.com>
2018-09-26 21:58:39 -0500

    first commit

---
+++ app/src/main/java/com/example/hp/tourist/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.hp.tourist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnIngresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    public void init(){
+        btnIngresar = findViewById(R.id.btnIngresar);
+        btnIngresar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int v = view.getId();
+        switch (v){
+            case R.id.btnIngresar:{
+                startActivity(new Intent(getApplicationContext(), NavigationActivity.class));
+                break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hp/tourist/NavigationActivity.java
@@ -0,0 +1,119 @@
+package com.example.hp.tourist;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+public class NavigationActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        FrameLayout fl = findViewById(R.id.drawer_layout);
+        fl.removeAllViews();
+        manager.popBackStack();
+        getFragmentManager().popBackStack();
+        Fragment fragment = null;
+
+
+
+        switch (item.getItemId()) {
+            case R.id.nav_camera: {
+
+                break;
+            }
+            case R.id.nav_gallery: {
+
+                break;
+            }
+            case R.id.nav_slideshow: {
+
+                break;
+            }
+            case R.id.nav_manage: {
+
+                break;
+            }
+            case R.id.nav_send: {
+
+                break;
+            }
+            case R.id.nav_share: {
+
+                break;
+            }
+        }
+
+
+        if(fragment != null) {
+            transaction.replace(R.id.drawer_layout, fragment);
+            transaction.commit();
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+}
